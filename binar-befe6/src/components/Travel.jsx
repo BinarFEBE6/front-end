ec91e9c1ef30590c49d3fc58f1d98da1d736aa4a
azkinf16 <azkifajri16@gmail.com>
2022-11-23 01:02:46 +0700

    changing style and responsive and add button section

--- binar-befe6/src/components/Travel.jsx
+++ binar-befe6/src/components/Travel.jsx
@@ -3,24 +3,34 @@ import travel from "../assets/travel.png";
 
 function Travel() {
   return (
-    <div className="travel-section lg:grid lg:grid-cols-2 p-5 lg:px-36 lg:justify-items-center lg:items-center text-gray-700">
-      <div className="description  ">
-        <h1 className="text-2xl font-bold text-center lg:text-left ">
-          Why Everyone Flies
-        </h1>
-        <p className="text-md text-justify font-sans">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
-          sint aliquid est reprehenderit culpa autem nobis laboriosam soluta
-          consectetur accusantium? Hic veniam earum neque adipisci cumque, ad
-          sequi perferendis deserunt voluptatum, reprehenderit vel quia tempore
-          ex aperiam. Ullam iusto autem, suscipit veritatis officia iure
-          deleniti?
-        </p>
+    <>
+      <div className="travel-section lg:grid lg:grid-cols-2 p-5 lg:px-36 lg:justify-items-center lg:items-center text-gray-700 mt-5 ml-16">
+        <div className="description">
+          <h1 className="text-2xl font-bold text-center lg:text-left ">
+            Why Everyone Flies
+          </h1>
+          <p className="text-md text-justify font-sans">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
+            sint aliquid est reprehenderit culpa autem nobis laboriosam soluta
+            consectetur accusantium? Hic veniam earum neque adipisci cumque, ad
+            sequi perferendis deserunt voluptatum, reprehenderit vel quia
+            tempore ex aperiam. Ullam iusto autem, suscipit veritatis officia
+            iure deleniti?
+          </p>
+        </div>
+        <div className="image flex justify-center items-center">
+          <img src={travel} alt="" className="w-[70%]" />
+        </div>
       </div>
-      <div className="image flex justify-center items-center">
-        <img src={travel} alt="" className="w-[75%] lg:w-[50%]" />
+      <div className="border border-1 border-slate-400 w-3/4 mx-auto h-36 my-12 rounded-3xl">
+        <div className="flex w-full justify-between p-16">
+          <h1>Button</h1>
+          <h1>Button</h1>
+          <h1>Button</h1>
+          <h1>Button</h1>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
