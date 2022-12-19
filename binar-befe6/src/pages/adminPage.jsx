26926f26debae712f84940017d2cfa260fc96a50
FauziIrhamPulungan <fauzyirham24@gmail.com>
2022-12-19 22:08:14 +0700

    edit admin

--- binar-befe6/src/pages/adminPage.jsx
+++ binar-befe6/src/pages/adminPage.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 function AdminPage() {
   const navigate = useNavigate();
-  return <div onClick={() => navigate("/")}>AdminPage</div>;
+  return <div onClick={() => navigate("/")}>hay</div>;
 }
 
 export default AdminPage;
